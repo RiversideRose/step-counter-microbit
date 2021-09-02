86d262f2499bf5184ab00b57a130065fd53fd886
Riverside <66091556+RiversideRose@users.noreply.github.com>
2021-09-02 10:55:57 -0700

    Added record storage and a few refinements.

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(steps)
+    basic.showString("Steps!")
     basic.pause(2000)
     basic.clearScreen()
 })
@@ -8,12 +9,21 @@ input.onButtonPressed(Button.AB, function () {
     basic.showString("FT")
 })
 input.onButtonPressed(Button.B, function () {
+    record = steps
     steps = 0
     basic.showString("Reset Steps to 0!")
+    if (record > oldsteps) {
+        basic.showString("New Record!")
+    }
 })
 input.onGesture(Gesture.Shake, function () {
     steps += 1
 })
+let oldsteps = 0
+let record = 0
 let steps = 0
 steps = 0
 basic.showString("Started!")
+loops.everyInterval(10000, function () {
+    oldsteps = steps
+})
